c7e391f696be03d49c24289ff65e8f3c6ff3ebf0
Daniel Gallo <daniel.gallo@xodusgroup.com>
2025-07-22 16:41:35 +0800

    index extraction

--- Surge Volume.py
+++ Surge Volume.py
@@ -12,4 +12,19 @@ branch = readjson.get('Branches')
 
 tpl = [fa.Tpl(f"{working_location}\\{case}.tpl") for case in cases]
 
+def variables_index():
+    indexes = {}
+    filtered_tpl = list(tpl[0].filter_trends('QLTWT ').items())
+    for item in filtered_tpl:
+        values = item[1].split("'")
+        if values[0] == 'QLTWT ' and values[5] == branch.upper():
+            indexes[f"{values[17]}"] = item [0]
+
+    return indexes
+indexes = variables_index()
+
+
+
+
+
 a=5
