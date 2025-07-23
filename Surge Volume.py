79ee159a513d46b3ca01230165aa4a30d1992c6d
Daniel Gallo <daniel.gallo@xodusgroup.com>
2025-07-23 16:50:12 +0800

    Index updated to work woth positions

--- Surge Volume.py
+++ Surge Volume.py
@@ -8,21 +8,45 @@ with open('main.json', 'r') as file:
 
 working_location = readjson.get('Working Location')   
 cases = readjson.get('Cases')
-branch = readjson.get('Branches') 
+model_inputs = readjson.get("Model Inputs")
+
 
 tpl = [fa.Tpl(f"{working_location}\\{case}.tpl") for case in cases]
 
 def variables_index():
     indexes = {}
-    filtered_tpl = list(tpl[0].filter_trends('QLTWT ').items())
-    for item in filtered_tpl:
-        values = item[1].split("'")
-        if values[0] == 'QLTWT ' and values[5] == branch.upper():
-            indexes[f"{values[17]}"] = item [0]
+    if model_inputs['Pipe']:
+        filtered_QLTWT = list(tpl[0].filter_trends('QLTWT ').items())
+        for item in filtered_QLTWT:
+            values = item[1].split("'")
+            if values[0] == 'QLTWT ' and values[5] == model_inputs['Branch'].upper() and values[9] == model_inputs['Pipe'] and values[13] == model_inputs['Section']:
+                indexes['Volumetric flow rate water'] = item [0]
+
+        filtered_QLTHL = list(tpl[0].filter_trends('QLTHL ').items())
+        for item in filtered_QLTHL:
+            values = item[1].split("'")
+            if values[0] == 'QLTHL ' and values[5] == model_inputs['Branch'].upper() and values[9] == model_inputs['Pipe'] and values[13] == model_inputs['Section']:
+                indexes['Volumetric flow rate oil'] = item [0]
+    elif model_inputs['Position']:
+        filtered_QLTWT = list(tpl[0].filter_trends('POSITION:').items())
+        for item in filtered_QLTWT:
+            values = item[1].split("'")
+            if values[0] == 'QLTWT ' and values[5] == model_inputs['Branch'].upper() and values[9] == model_inputs['Pipe'] and values[13] == model_inputs['Section']:
+                indexes['Volumetric flow rate water'] = item [0]
+
+        filtered_QLTHL = list(tpl[0].filter_trends('QLTHL ').items())
+        for item in filtered_QLTHL:
+            values = item[1].split("'")
+            if values[0] == 'QLTHL ' and values[5] == model_inputs['Branch'].upper() and values[9] == model_inputs['Pipe'] and values[13] == model_inputs['Section']:
+                indexes['Volumetric flow rate oil'] = item [0]
+
+
 
     return indexes
 indexes = variables_index()
 
+
+tpl.extract(indexes)
 
 
 
